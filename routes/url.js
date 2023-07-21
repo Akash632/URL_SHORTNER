9bb6338cb5617ada3952db27caee8e909172f880
Akash Nagineni <akashnagineni632@gmail.com>
2023-07-21 16:06:00 +0530

    first commit

---
+++ routes/url.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const {
+  handleGenerateNewShortURL, handleUpdateURL,
+} = require("../controllers/url");
+
+const router = express.Router();
+
+router.post("/", handleGenerateNewShortURL);
+
+router.post("/update/:short_url",handleUpdateURL);
+
+module.exports = router;
